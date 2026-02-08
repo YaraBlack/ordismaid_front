8aeb0e346e109c6e1857f6a10a60e8d84e9e3716
YaraBlack <yara26072000@gmail.com>
2026-02-09 00:20:43 +0100

    test back-end data fetching

--- src/pages/Timers/index.tsx
+++ src/pages/Timers/index.tsx
@@ -1,3 +1,26 @@
+import { useTimers } from "../../hooks/useTimers";
+import { Alert } from "../../interfaces/alert";
+
 export const Timers = () => {
-  return <div>Bunch of timers</div>;
+  const { data, loading, fetchData } = useTimers();
+  console.log(data.data);
+
+  if (loading) return <div>Loading initial data...</div>;
+
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h1>Timers</h1>
+        <button onClick={fetchData}>Refresh Now</button>
+      </div>
+      <div>
+        <h2>Alerts</h2>
+        <ul>
+          {data.data.alerts.map((item: Alert) => (
+            <li key={item.id}>{item.expiry}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
