49a13923e46f3f306628bbcec1fe7d25e273f19e
YaraBlack <yara26072000@gmail.com>
2026-02-14 21:45:53 +0100

    error handling

--- src/pages/Timers/index.tsx
+++ src/pages/Timers/index.tsx
@@ -2,10 +2,19 @@ import { useTimers } from "../../hooks/useTimers";
 import { Alert } from "../../interfaces/alert";
 
 export const Timers = () => {
-  const { data, loading, fetchData } = useTimers();
-  console.log(data.data);
+  const { data, loading, error, fetchData } = useTimers();
+  console.log(data, error);
+  console.log(data?.data.alerts);
 
   if (loading) return <div>Loading initial data...</div>;
+
+  if (!data && error)
+    return (
+      <div>
+        <p>Server unavailable.</p>
+        <button onClick={fetchData}>Retry</button>
+      </div>
+    );
 
   return (
     <div>
@@ -16,7 +25,7 @@ export const Timers = () => {
       <div>
         <h2>Alerts</h2>
         <ul>
-          {data.data.alerts.map((item: Alert) => (
+          {data?.data.alerts.map((item: Alert) => (
             <li key={item.id}>{item.expiry}</li>
           ))}
         </ul>
