ea68f33500bc04fe1bbd41f0b5eec3dee333f623
YaraBlack <yara26072000@gmail.com>
2026-02-14 21:44:08 +0100

    changed mission and rewardTypes types

--- src/interfaces/alert.ts
+++ src/interfaces/alert.ts
@@ -1,8 +1,10 @@
+import { Mission } from "./mission";
+
 export interface Alert {
-    id: string | number;
-    activation: string;
-    expiry: string;
-    mission: {};
-    rewardTypes: [];
-    tag: string;
+  id: string | number;
+  activation: string;
+  expiry: string;
+  mission: Mission;
+  rewardTypes: any[];
+  tag: string;
 }
