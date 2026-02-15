c0eba565942897dcb714d7bc180329950ff85e79
YaraBlack <yara26072000@gmail.com>
2026-02-15 03:17:34 +0100

    add date formatter with days

--- src/tools/dateFormatter.ts
+++ src/tools/dateFormatter.ts
@@ -10,3 +10,17 @@ export const formatTimeLeft = (expiry: string) => {
     .map((v) => v.toString().padStart(2, "0"))
     .join(":");
 };
+
+export const formatTimeLeftWithDays = (start: number, end: number) => {
+  const difference = start - end;
+  if (difference <= 0) return null;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((difference / 1000 / 60) % 60);
+  const seconds = Math.floor((difference / 1000) % 60);
+
+  return [days, hours, minutes, seconds]
+    .map((v) => v.toString().padStart(2, "0"))
+    .join(":");
+};
